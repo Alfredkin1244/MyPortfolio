760f454e4bd5a59fe8901363205f1136b0bdb9a8
Julian Alfredo Aguilar <146030511+Alfredkin1244@users.noreply.github.com>
2026-01-06 18:15:15 -0600

    fix react version and react-dom version

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+// reportWebVitals();
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // O el nombre de tu CSS principal
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
-
-reportWebVitals();
